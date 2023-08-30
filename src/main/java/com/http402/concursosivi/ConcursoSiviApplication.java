a9010fbeb09219f1e81d70f5a954c52f1270de5c
Rail Neira <r.neira03@ufromail.cl>
2023-08-30 11:53:09 -0400

    Commit inicial

---
+++ src/main/java/com/http402/concursosivi/ConcursoSiviApplication.java
@@ -0,0 +1,13 @@
+package com.http402.concursosivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcursoSiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcursoSiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/http402/concursosivi/ConcursoSiviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.http402.concursosivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcursoSiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
